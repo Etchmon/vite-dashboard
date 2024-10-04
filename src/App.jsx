d091b5d495119841b1c960d1e18b8a409361f84f
James Etchells <Etch.rush@gmail.com>
2024-10-04 10:39:21 -0700

    Add Dashboard to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Dashboard />
     </Router>
   );
 }
