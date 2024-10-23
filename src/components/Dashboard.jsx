9a82129a5837d30c20b549313996acf808f62b4f
Etchmon <etch@jamesetchells.com>
2024-10-23 12:35:53 -0700

    EDIT started sidebar project

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -23,15 +23,9 @@ function Dashboard() {
     { label: 'April', value: 80 },
   ];
   return (
-    <Flex bg="catppuccin.surface0" height="100vh" width="100vw">
-      <Box width="100%" bg="catppuccin.surface0" color="catpuccin.text" p="4" textAlign={['center']}>
+    <Flex bg="catppuccin.surface0" height="100vh">
+      <Box  bg="catppuccin.surface0" color="catpuccin.text" p="4" textAlign={['center']}>
         <Grid templateColumns={{ base:'repeat(1, 1fr)', md:'repeat(2, 1fr)'}} gap={6}>
-          <GridItem>
-            <BarChartComponent data={data}/>
-          </GridItem>
-          <GridItem>
-            <BarChartComponent data={data}/>
-          </GridItem>
         </Grid>
       </Box>
     </Flex>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,34 +4,19 @@ function Navbar() {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box bg="catppuccin.base" p={4}>
+    <Box bg="catppuccin.base" p={4} width={{base:'100%', lg:'300px'}} height={{lg:'100vh'}} position={{lg:'fixed'}}>
       <Flex justify="space-between" align="center">
         <Box color="white" fontSize="lg" fontWeight="bold">My Dashboard</Box>
         <Button
           colorScheme="teal"
-          display={['block', 'none']}
+          display={['block', 'block', 'none']}
           onClick={onToggle}
         >
           Menu
         </Button>
-        <Stack
-          direction="row"
-          spacing={4}
-          display={{ base: 'none', md: 'flex'}}
-        >
-          <Button colorScheme="teal" variant="outline">
-            Dashboard
-          </Button>
-           <Button colorScheme="teal" variant="outline">
-            Settings 
-          </Button>
-          <Button colorScheme="teal" variant="outline">
-            Profile 
-          </Button>
-        </Stack>
       </Flex>
       {isOpen && (
-        <Flex direction="column" bg="gray.700" mt={2} display={['flex','none']}>
+        <Flex direction="column" bg="gray.700" mt={2} display={['flex','flex', 'none']}>
           <Button colorScheme="teal" mb={2}>Dashboard</Button>
           <Button colorScheme="teal">Settings</Button>
         </Flex>
