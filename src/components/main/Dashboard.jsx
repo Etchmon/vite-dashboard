f04aa035766fecbc79424ebd36ecb947501422af
etchmac <etch@Etchs-MacBook-Pro.local>
2024-11-23 14:30:50 -0800

    EDIT: removed unused imports, applied formatter

--- src/components/main/Dashboard.jsx
+++ src/components/main/Dashboard.jsx
@@ -1,13 +1,11 @@
-import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
-import BarChartComponent from './charts/BarChart';
-import { getTopCoins } from '../services/geckoService';
+import { Box, Flex, Grid } from "@chakra-ui/react";
 
 function Dashboard() {
 
   return (
     <Flex bg="catppuccin.surface0" height="100vh">
-      <Box  bg="catppuccin.surface0" color="catpuccin.text" p="4" textAlign={['center']}>
-        <Grid templateColumns={{ base:'repeat(1, 1fr)', md:'repeat(2, 1fr)'}} gap={6}>
+      <Box bg="catppuccin.surface0" color="catpuccin.text" p="4" textAlign={['center']}>
+        <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }} gap={6}>
         </Grid>
       </Box>
     </Flex>
