541a80e8dac1e119c135ed7716b344ce50d98dba
etchmac <etch@Etchs-MacBook-Pro.local>
2024-11-28 12:16:11 -0800

    EDIT: Button position, removed unused imports

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Button, Stack, useDisclosure } from '@chakra-ui/react'
+import { Box, Flex, Button, useDisclosure } from '@chakra-ui/react'
 import SidebarContent from './SidebarContent.jsx';
 
 function Navbar() {
@@ -12,22 +12,22 @@ function Navbar() {
         <Box
           bg="catppuccin.base"
           pg={4}
-          width={{ base: '100%', lg: '300px'}}
-          height={{ base: 'auto', lg: '100vh'}}
-          position={{ lg: 'fixed'}}
-          overflowY={{ lg: 'auto'}}
-          display={{ base: isOpen ? 'block' : 'none', lg: 'block'}}
-          zIndex={{ base: 'overlay', lg: 'auto' }} 
+          width={{ base: '100%', lg: '300px' }}
+          height={{ base: 'auto', lg: '100vh' }}
+          position={{ lg: 'fixed' }}
+          overflowY={{ lg: 'auto' }}
+          display={{ base: isOpen ? 'block' : 'none', lg: 'block' }}
+          zIndex={{ base: 'overlay', lg: 'auto' }}
         >
           <SidebarContent onClose={onClose} />
         </Box>
 
         { /* Menu Button for Small Screens */}
-        <Box 
-          display={{ base: 'block', lg: 'none'}}
+        <Box
+          display={{ base: 'block', lg: 'none' }}
           position="fixed"
           top="4"
-          left="4"
+          right="4"
           zIndex="overlay"
         >
           <Button colorScheme="teal" onClick={onToggle}>
