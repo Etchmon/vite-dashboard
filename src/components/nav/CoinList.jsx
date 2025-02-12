8d8adefbf9431ddb3d4b80d5358f1223b0d9168c
etchmac <etch@Etchs-MacBook-Pro.local>
2025-02-12 15:08:55 -0800

    ADD: error handling and loading spinner to CoinList component

--- src/components/nav/CoinList.jsx
+++ src/components/nav/CoinList.jsx
@@ -1,30 +1,58 @@
-import { SimpleGrid, Box } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { getTopCoins } from '../../services/geckoService.js';
-import CoinCard from './CoinCard.jsx';
+import { SimpleGrid, Box, Spinner, Alert, AlertIcon } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { getTopCoins } from "../../services/geckoService.js";
+import CoinCard from "./CoinCard.jsx";
 
 const CoinList = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getTopCoins();
-      setCoins(data);
+      try {
+        const data = await getTopCoins();
+        setCoins(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
   }, []);
 
-  console.log(coins)
+  console.log(coins);
+
+  if (loading) {
+    return <Spinner size="xl" />;
+  }
+
+  if (error) {
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        There was an error processing your request.
+      </Alert>
+    );
+  }
+
   return (
     <SimpleGrid columns={1} spacing={4}>
       {coins.map((coin) => (
-        <Box key={coin.id} p={4} boxShadow="md" borderRadius="md" bg="catpuccino.surface1">
+        <Box
+          key={coin.id}
+          p={4}
+          boxShadow="md"
+          borderRadius="md"
+          bg="catpuccino.surface1"
+        >
           <CoinCard coin={coin} />
         </Box>
       ))}
     </SimpleGrid>
   );
-}
+};
 
 export default CoinList;
