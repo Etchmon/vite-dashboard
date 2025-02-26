fa047b46a8b85dd7c644c1422ba3fcf0e5ea0a9f
Etchmon <etch@jamesetchells.com>
2025-02-26 12:27:52 -0800

    EDIT: Tickerbar now scrolls correctly

--- src/components/ticker/TickerBar.jsx
+++ src/components/ticker/TickerBar.jsx
@@ -2,23 +2,19 @@ import { Box, Text, Flex, keyframes } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { getTopCoins } from "../../services/geckoService.js";
 
-const scroll = keyframes`
-    0% {
-        transform: translateX(0);
-    }
-    100% {
-        transform: translateX(-100%);
-    }
-`;
-
 const TickerBar = () => {
   const [coins, setCoins] = useState([]);
+  const [duration, setDuration] = useState(12);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getTopCoins();
         setCoins(data);
+
+        // Calculate duration based on the number of coins
+        const coinCount = data.length;
+        setDuration(coinCount * 2);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -26,6 +22,15 @@ const TickerBar = () => {
 
     fetchData();
   }, []);
+
+  const scroll = keyframes`
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(-50%);
+    }
+`;
 
   return (
     <Box
@@ -35,10 +40,13 @@ const TickerBar = () => {
       whiteSpace="nowrap"
       bg="catpuccino.surface1"
     >
-      <Flex animation={`${scroll} 12s linear infinite`}>
-        {coins.map((coin) => (
+      <Flex
+        width="fit-content"
+        animation={`${scroll} ${duration}s linear infinite`}
+      >
+        {[...coins, ...coins].map((coin, index) => (
           <Text
-            key={coin.id}
+            key={`${coin.id}-${index}`}
             p={4}
             color={
               coin.price_change_percentage_24h >= 0 ? "green.500" : "red.500"
