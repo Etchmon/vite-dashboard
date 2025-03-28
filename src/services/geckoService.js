45156167fc15f7c16daac7961ad699abb31b0251
Etchmon <etch@jamesetchells.com>
2025-03-28 13:11:41 -0700

    ADD: comments to geckService

--- src/services/geckoService.js
+++ src/services/geckoService.js
@@ -3,19 +3,19 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const cache = new Map(); // Initialize cache
 const cacheKey = "topCoins";
 const fetchInterval = 2 * 60 * 1000; // 2 minutes
+// const updateInterval = 24 * 60 * 60 * 1000; // 24 hours
 
 export const fetchTopCoins = async () => {
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&x_cg_demo_api_key=${apiKey}`;
 
+  // Function to fetch data from the API
   try {
     console.log("Fetching top coins from API at", new Date().toISOString());
-    // Fetch data
     const response = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
       },
     });
-    // Check if response is ok
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -35,6 +35,9 @@ fetchTopCoins();
 // Set up interval to fetch data every 2 minutes
 setInterval(fetchTopCoins, fetchInterval);
 
+// Function checks if the cache is empty or if the data is older than the fetch interval
+// If so, it fetches new data from the API
+// Otherwise, it returns the cached data
 export const getTopCoins = async () => {
   if (cache.has(cacheKey)) {
     console.log("Fetching cached data at:", new Date().toISOString());
@@ -43,3 +46,5 @@ export const getTopCoins = async () => {
     return fetchTopCoins();
   }
 };
+
+// Function to fetch historical market data for specific coin
